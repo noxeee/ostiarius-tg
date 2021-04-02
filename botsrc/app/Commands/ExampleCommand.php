23f0913d9adc06ca73aef39e1fbc317c811473d1
Lujza Lanyi <lujzal97@unet.univie.ac.at>
2021-04-02 13:35:27 +0200

    an example command to copy from while creating new commands

---
+++ botsrc/app/Commands/ExampleCommand.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Commands;
+
+use BotMan\BotMan\Messages\Incoming\Answer;
+use BotMan\BotMan\Messages\Outgoing\Question;
+use BotMan\BotMan\Messages\Outgoing\Actions\Button;
+use BotMan\BotMan\Messages\Conversations\Conversation;
+
+class ExampleCommand {
+    
+}
